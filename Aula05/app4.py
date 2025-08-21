8a55e07d8f2ec385cbc5dd00bc998d4f236aabe7
karython <karython.unai@gmail.com>
2025-08-21 12:30:16 -0300

    aula 05 21/08/25

---
+++ Aula05/app4.py
@@ -0,0 +1,40 @@
+import os
+
+while True:
+    try:
+        novo_texto = input('Digite o texto: \n')
+        nome_arquivo = input('Digite o nome do arquivo (sem extensão): ').strip().lower()
+
+        with open(fr'C:\Users\ead\Documents\gomes\Nem 2 serie\logica programação\LogicaAulas\Aula05{nome_arquivo}.txt', 'w', encoding='utf-8') as f:
+            f.write(novo_texto)
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'{nome_arquivo}.txt gravado com sucesso.')
+
+
+        novo_texto_adicionar = input('Digite o novo texto: \n')
+
+        with open(fr'C:\Users\ead\Documents\gomes\Nem 2 serie\logica programação\LogicaAulas\Aula05{nome_arquivo}.txt', 'a', encoding='utf-8') as f:
+            f.write(f'\n{novo_texto_adicionar}')
+        print('Gravado com sucesso.')
+
+        # ler os dados atualizados
+        with open(f'{nome_arquivo}.txt', 'r', encoding='utf-8') as f:
+            texto_final = f.read()
+        print(f'Texto final: {texto_final}')
+
+        while True:
+            prosseguir = input('Deseja abrir outro arquivo? (s/n): ').strip().lower()
+            if prosseguir == 's' or prosseguir == 'n':
+                break
+            else:
+                print('Opção inválida!')
+                continue
+        match prosseguir:
+            case 's':
+                continue
+            case 'n':
+                break
+    except Exception as e:
+        print(f'Não foi possivel ler o arquivo. {e}')
+        continue
